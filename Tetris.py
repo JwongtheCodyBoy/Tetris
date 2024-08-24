75f04c83cc52569e5b2aba2a80e2c95e2f2e3cb4
Jonathan Wong <Xmexybutt@gmail.com>
2024-08-24 00:28:53 -0400

    Tetris done

--- Tetris.py
+++ Tetris.py
@@ -29,14 +29,28 @@ tetrominos = [
     for block_pos in tetrominos_pos
 ]
 tetromino_rect = pygame.Rect(0,0, TILE -2, TILE -2)
-tetromino = deepcopy(choice(tetrominos))
+tetromino, next_tetromino = deepcopy(choice(tetrominos)), deepcopy(choice(tetrominos))
 
 feild = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
 
 anim_count, anim_speed, anim_limit = 0, 60, 2000
 
+title_font = pygame.font.Font('HANGTHEDJ.ttf', 65)
+font = pygame.font.Font('HANGTHEDJ.ttf', 45)
+gameOverFont = pygame.font.Font('HANGTHEDJ.ttf', 100)
+
+title_tetris = title_font.render('TETRIS', True, pygame.Color('darkorange'))
+title_score = font.render('score:', True, pygame.Color('green'))
+title_HIGH = font.render('High', True, pygame.Color('purple'))
+title_RECORD = font.render('Score:', True, pygame.Color('purple'))
+
+GameOverTxt = gameOverFont.render('GAME OVER', True, pygame.Color('firebrick1'))
+
 bg = pygame.image.load('img/bg.jpg').convert()
 game_bg = pygame.image.load('img/bg2.jpg').convert()
+
+score, lines = 0, 0
+scores = {0:0, 1:100, 2:300, 3:700, 4:1500}
 
 
 def check_borders():
@@ -46,7 +60,22 @@ def check_borders():
         return False
     return True
 
+def get_record():
+    try:
+        with open('record.txt') as f:
+            return f.readline()
+    except FileNotFoundError:
+        with open('record.txt', 'w') as f:
+            f.write('0')
+
+def set_record(record, score):
+    rec = max(int(record), score)
+    with open('record.txt', 'w') as f:
+        f.write(str(rec))
+
+death_flag = False
 while True:
+    record = get_record()
     dir_x = 0
     rotate = False
 
@@ -54,6 +83,11 @@ while True:
     sc.blit(screen, (20,20))
     screen.blit(game_bg, (0,0))
 
+    # Delay for full lines
+    for i in range(lines):
+        pygame.time.wait(200)
+
+    # Controls
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -71,6 +105,8 @@ while True:
             if event.key == pygame.K_DOWN:
                 anim_limit = 2000
     
+    if death_flag: continue
+
     # x movement
     tetromino_old = deepcopy(tetromino)
     for i in range(4):
@@ -89,7 +125,8 @@ while True:
             if not check_borders():
                 for i in range(4):
                     feild[tetromino_old[i].y][tetromino_old[i].x] = tetromino[4]
-                tetromino = deepcopy(choice(tetrominos))
+                tetromino = next_tetromino
+                next_tetromino = deepcopy(choice(tetrominos))
                 break
 
     # Rotating
@@ -105,7 +142,8 @@ while True:
                 tetromino = deepcopy(tetromino_old)
                 break
 
-    line = HEIGHT - 1
+    # Checking Lines 
+    line, lines = HEIGHT - 1, 0
     for row in range(HEIGHT - 1, -1, -1):
         count = 0
         for i in range(WIDTH):
@@ -114,6 +152,12 @@ while True:
             feild[line][i] = feild[row][i]
         if count < WIDTH:
             line -= 1
+        else: 
+            anim_speed += 3
+            lines += 1
+    # Compute Score
+    score += scores[lines]
+     
 
     # (40,40,40) is color (gray), i_rect is position in GRID, [] is used because we are running this line by making a list and not storing list in a var so no new element is created (could just write it normamly but python is cool like that)
     [pygame.draw.rect(screen, (40,40,40), i_rect, 1) for i_rect in GRID]
@@ -130,6 +174,29 @@ while True:
             if col:
                 tetromino_rect.x, tetromino_rect.y = x * TILE, y * TILE
                 pygame.draw.rect(screen, col, tetromino_rect)
+
+    # Draw next figure
+    for i in range(4):
+        tetromino_rect.x = next_tetromino[i].x * TILE + 375
+        tetromino_rect.y = next_tetromino[i].y * TILE + 185
+        pygame.draw.rect(sc, next_tetromino[4], tetromino_rect)
+
+    # Draw Titles
+    sc.blit(title_tetris, (475, 20))
+    sc.blit(title_score, (535, 780))
+    sc.blit(font.render(str(score), True, pygame.Color('white')), (550, 840))
+    sc.blit(title_HIGH, (525, 600))
+    sc.blit(title_RECORD, (525, 650))
+
+    record = record if int(record) > score else score
+    sc.blit(font.render(str(record), True, pygame.Color('gold')), (550, 710))
+
+    # Game Over
+    for i in range(WIDTH):
+        if feild[0][i]:
+            set_record(record,score)
+            death_flag = True
+            sc.blit(GameOverTxt, (WIDTH * TILE/8, HEIGHT * TILE/2))
 
     pygame.display.update()
     CLOCK.tick(60)
